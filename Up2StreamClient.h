6f96f15f5e7fa0a33897ce19807bf7dbb743b291
wbrzezin <waldemar@brzezinski.waw.pl>
2026-08-04 22:53:30 +0200

    Create Up2StreamClient.h

---
+++ Up2StreamClient.h
@@ -0,0 +1,77 @@
+//==============================================================
+// Projekt : UP2Stream Display
+// Plik    : Up2StreamClient.h
+//
+// Opis:
+//
+// Klasa odpowiedzialna za komunikację UART z modułem
+// Arylic Up2Stream.
+//
+// Zadania klasy:
+//
+// • odbiór danych z UART,
+// • składanie komunikatów,
+// • analiza protokołu,
+// • aktualizacja struktury PlayerState,
+// • zgłaszanie zmian do modułu Display.
+//
+// Klasa nie rysuje niczego na ekranie.
+//==============================================================
+
+#ifndef UP2STREAMCLIENT_H
+#define UP2STREAMCLIENT_H
+
+//--------------------------------------------------------------
+// Biblioteki.
+//--------------------------------------------------------------
+
+#include <Arduino.h>
+
+#include "PlayerState.h"
+
+#include "ChangeFlags.h"
+
+
+//==============================================================
+// Klasa Up2StreamClient
+//==============================================================
+
+class Up2StreamClient
+{
+public:
+
+    //----------------------------------------------------------
+    // Konstruktor.
+    //----------------------------------------------------------
+
+    Up2StreamClient();
+
+
+    //----------------------------------------------------------
+    // Inicjalizacja komunikacji UART.
+    //----------------------------------------------------------
+
+    void begin(HardwareSerial& serial);
+
+
+    //----------------------------------------------------------
+    // Aktualizacja.
+    //
+    // Funkcja odczytuje dane z UART i zwraca informacje
+    // o zmianach wykrytych w stanie odtwarzacza.
+    //----------------------------------------------------------
+
+    ChangeFlags update(PlayerState& player);
+
+
+private:
+
+    //----------------------------------------------------------
+    // Wskaźnik do portu UART.
+    //----------------------------------------------------------
+
+    HardwareSerial* uart;
+
+};
+
+#endif
